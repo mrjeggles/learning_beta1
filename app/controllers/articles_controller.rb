6982c966b7d672d9a1b2787d7dc8902ef8d1afcc
Warwick <w.paton@gmail.com>
2016-08-25 15:55:27 +1000

    putting an order into the index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @articles = Article.all.order("subject ASC, topic ASC")
   end
 
   # GET /articles/1
