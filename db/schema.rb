83f2a704cbf5fb9b2ee01cc6477480e6a408c0e2
Warwick <w.paton@gmail.com>
2016-08-30 20:54:05 +1000

    end of chapter 6

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160828112606) do
+ActiveRecord::Schema.define(version: 20160830041558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20160828112606) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
+    t.string   "hashed_password"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
